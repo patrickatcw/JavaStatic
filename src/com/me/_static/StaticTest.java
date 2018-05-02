b3bfdb369f68dee8175d2f1d46ebc679357f1da5
patrickatcw <pj8392@yahoo.com>
2018-05-02 10:29:58 -0400

    complete here, now to main

---
+++ src/com/me/_static/StaticTest.java
@@ -0,0 +1,28 @@
+package com.me._static;
+
+//step 1 class
+public class StaticTest {
+
+    //step 2 variables / fields
+    private int numInstances = 0;
+    private String name;
+
+    //step 3 constructor for just name
+    public StaticTest(String name) {
+        this.name = name;
+        //step 4 to iterate
+        numInstances++;
+    }
+
+    //step 5 getter for both
+    public int getNumInstances() {
+        return numInstances;
+    }
+
+    public String getName() {
+        return name;
+
+        //step 6 to main
+
+    }
+}
